40ccb0afbbf5143f67c2ffbdbdb791476b78f621
Owen Harris <owenharris21@icloud.com>
2018-09-21 18:26:09 -0600

    update

--- EventTrackerREST/src/main/webapp/js/script.js
+++ EventTrackerREST/src/main/webapp/js/script.js
@@ -19,31 +19,80 @@ window.addEventListener('load', function(e) {
 function displayNutrition(nutrition) {
 	var dataDiv = document.getElementById('startTable');
 	
-	// draw table out and create elements 
 	var table = document.createElement('table'); 
-	var th = document.createElement('th'); 
-	var tr = document.createElement('tr'); 
-	var td = document.createElement('td'); 
+		var thead = document.createElement('thead'); 
+			var tr = document.createElement('tr'); 
+			
+				var thFname = document.createElement('th'); 
+				thFname.textContent = "First Name";
+				
+				var thLname = document.createElement('th'); 
+				thLname.textContent = "Last Name";
+				
+				var thCurWeight = document.createElement('th'); 
+				thCurWeight.textContent = "Current Weight";
+				
+				var thDesWeight = document.createElement('th'); 
+				thDesWeight.textContent = "Desired Weight";
+				
+				var thProtein = document.createElement('th'); 
+				thProtein.textContent = "Protein";
+				
+				var thCarbs = document.createElement('th'); 
+				thCarbs.textContent = "Carbs";
+				
+				var thFat = document.createElement('th'); 
+				thFat.textContent = "Fat";
+				
+				var thNotes = document.createElement('th'); 
+				thNotes.textContent = "Notes";
+				
+				table.appendChild(thead); 
+				thead.appendChild(tr); 
+				tr.appendChild(thFname); 
+				tr.appendChild(thLname); 
+				tr.appendChild(thCurWeight); 
+				tr.appendChild(thDesWeight); 
+				tr.appendChild(thProtein); 
+				tr.appendChild(thCarbs); 
+				tr.appendChild(thFat); 
+				tr.appendChild(thNotes); 
 	
-	table.id = 'nutritionTable';
-	table.appendChild('th'); 
-	th.textContent = "First Name";
-	th.textContent = "Last Name";
-	th.textContent = "Current Weight";
-	th.textContent = "Desired Weight";
-	th.textContent = "Protein";
-	th.textContent = "Carbs";
-	th.textContent = "Fat";
-	th.textContent = "Notes";
-	
-	th.appendChild('tr'); 
-	tr.appendChild('td'); 
-	td.textContent = nutrition.firstName; 
-	td.textContent = nutrition.lastName; 
-	td.textContent = nutrition.currentWeight; 
-	td.textContent = nutrition.desiredWeight; 
-	td.textContent = nutrition.protein; 
-	td.textContent = nutrition.carbs; 
-	td.textContent = nutrition.fat; 
-	td.textContent = nutrition.notes; 
+		var tbody = document.createElement('tbody'); 
+			var tr = document.createElement('tr'); 
+				
+				var fname = document.createElement('td'); 
+				fname.textContent = nutrition.firstName;
+				
+				var lname = document.createElement('td'); 
+				lname.textContent = nutrition.lastName;
+				
+				var curWeight = document.createElement('td'); 
+				curWeight.textContent = nutrition.currentWeight;
+				
+				var desWeight = document.createElement('td'); 
+				desWeight.textContent = nutrition.desiredWeight;
+				
+				var protein = document.createElement('td'); 
+				protein.textContent = nutrition.protein;
+				
+				var carbs = document.createElement('td'); 
+				carbs.textContent = nutrition.carbs;
+				
+				var fat = document.createElement('td'); 
+				fat.textContent = nutrition.fat;
+				
+				var notes = document.createElement('td'); 
+				notes.textContent = nutrition.notes;
+				
+				table.appendChild(tbody); 
+				tbody.appendChild(tr); 
+				tr.appendChild(fname); 
+				tr.appendChild(lname); 
+				tr.appendChild(curWeight); 
+				tr.appendChild(desWeight); 
+				tr.appendChild(protein); 
+				tr.appendChild(carbs); 
+				tr.appendChild(fat); 
+				tr.appendChild(notes); 
 }
