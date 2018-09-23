b58dac690f19763fcda1207601bdfde2305d971f
Owen Harris <owenharris21@icloud.com>
2018-09-23 17:46:23 -0600

    update

--- EventTrackerREST/src/main/webapp/js/script.js
+++ EventTrackerREST/src/main/webapp/js/script.js
@@ -11,19 +11,177 @@ window.addEventListener('load', function(e) {
 		}
 	}
 	xhr.send();
-	
 });
 
 function addUpdateAndDelete() {
 	var trList = document.getElementsByTagName("tr");
 	for (var i = 0; i < trList.length; i++) {
 		trList[i].addEventListener("click", function(e) {
-			// when row is clicked, display edit and delete options 
-			console.log("Row Clicked");
+			let row = e.target.parentElement; 
+			var array = []; 
+			for (var i = 0; i < row.children.length; i++) {
+				array[i] = row.children[i].textContent; 
+			}
+			detailView(array); 
 		});
 	}
 } 
 
+// create update and delete buttons and add event listeners 
+function detailView(selectedRow) {
+	
+	// print out selectedRow in a table with type = text so it can be edited 
+	var detailDiv = document.getElementById('detail');
+	var table = document.createElement('table');
+	var tr = document.createElement('tr');
+	detailDiv.appendChild(table); 
+	table.appendChild(tr);
+	
+	// needs to be textboxes so they can be edited 
+	var fname = document.createElement('td');
+	fname.type = "text"; 
+	var lname = document.createElement('td');
+	lname.type = "text"; 
+	var curWeight = document.createElement('td');
+	var desWeight = document.createElement('td');
+	var protein = document.createElement('td');
+	var carbs = document.createElement('td');
+	var fat = document.createElement('td');
+	var notes = document.createElement('td');
+		
+	fname.textContent = selectedRow[0];
+	lname.textContent = selectedRow[1];
+	curWeight.textContent = selectedRow[2];
+	desWeight.textContent = selectedRow[3];
+	protein.textContent = selectedRow[4];
+	carbs.textContent = selectedRow[5];
+	fat.textContent = selectedRow[6];
+	notes.textContent = selectedRow[7];
+		
+	tr.appendChild(fname); 
+	tr.appendChild(lname); 
+	tr.appendChild(curWeight); 
+	tr.appendChild(desWeight); 
+	tr.appendChild(protein); 
+	tr.appendChild(carbs); 
+	tr.appendChild(fat); 
+	tr.appendChild(notes); 
+	
+	// update
+	var update = document.createElement("input");
+	update.type = "submit";
+	update.value = "update"; 
+	update.name = "update"; 
+	update.id = "a"; 
+    var foo = document.getElementById("detail");
+    foo.appendChild(update);
+    
+    document.getElementById("a").addEventListener('click', function(e) {
+    	e.preventDefault();
+
+    	
+//    	var updatedRow = {
+//    		firstName : detail.firstElementChild.fname.value,
+//    		lastName : detail.firstElementChild.lname.value,
+//    		currentWeight : detail.firstElementChild.current.value,
+//    		desiredWeight : detail.firstElementChild.desired.value,
+//    		protein : detail.firstElementChild.protein.value,
+//    		carbs : detail.firstElementChild.carbs.value,
+//    		fat : detail.firstElementChild.fat.value,
+//    		notes : detail.firstElementChild.notes.value
+//    	}
+
+    	// send to Patch route
+    	var xhr = new XMLHttpRequest();
+    	xhr.open('PATCH', 'api/editnutrition', true);
+    	xhr.setRequestHeader("Content-type", "application/json"); 
+    	xhr.onreadystatechange = function() {
+    		if (xhr.readyState === 4) {
+    			if (xhr.status == 200 || xhr.status == 201) {
+    				var data = JSON.parse(xhr.responseText);
+    				console.log(data);
+    				
+    				// reload table with updated info 
+    				var xhr2 = new XMLHttpRequest();
+    				xhr2.open('GET', 'api/nutritioninfo');
+    				xhr2.onreadystatechange = function() {
+    					if (xhr2.readyState === 4 && xhr2.status === 200) {
+    						var allEvents = JSON.parse(xhr2.responseText);
+    						displayNutrition(allEvents);
+    					} else {
+    						console.log("*** Data not found ***");
+    					}
+    				}
+    				xhr2.send();
+    			} else {
+    				console.log("PATCH request failed.");
+    				console.error(xhr.status + ': ' + xhr.responseText);
+    			}
+    		}
+    	};
+    	var userObjectJson = JSON.stringify(updatedRow); 
+    	xhr.send(userObjectJson);
+    }); 
+    
+    // delete 
+    var del = document.createElement("input");
+    del.type = "submit";
+    del.value = "delete"; 
+    del.name = "delete"; 
+    del.id = "b"; 
+    var bar = document.getElementById("detail");
+    bar.appendChild(del);
+    
+    document.getElementById("b").addEventListener('click', function(e) {
+    	e.preventDefault();
+ 
+    	// need a JSON object 
+    	var rowToDelete = {
+    		firstName : selectedRow[0],
+        	lastName : selectedRow[1],
+        	currentWeight : selectedRow[2],
+        	desiredWeight : selectedRow[3],
+        	protein : selectedRow[4],
+        	carbs : selectedRow[5],
+        	fat : selectedRow[6],
+        	notes : selectedRow[7]
+    	}
+
+    	// send to Delete route
+    	var xhr = new XMLHttpRequest();
+    	xhr.open('DELETE', 'api/deletenutrition', true);
+    	xhr.setRequestHeader("Content-type", "application/json"); 
+    	xhr.onreadystatechange = function() {
+    		if (xhr.readyState === 4) {
+    			if (xhr.status == 200 || xhr.status == 201) {
+    				var data = JSON.parse(xhr.responseText);
+    				console.log(data);
+    				
+    				// reload table with updated info 
+    				var xhr2 = new XMLHttpRequest();
+    				xhr2.open('GET', 'api/nutritioninfo');
+    				xhr2.onreadystatechange = function() {
+    					if (xhr2.readyState === 4 && xhr2.status === 200) {
+    						var allEvents = JSON.parse(xhr2.responseText);
+    						displayNutrition(allEvents);
+    					} else {
+    						console.log("*** Data not found ***");
+    					}
+    				}
+    				xhr2.send();
+    			} else {
+    				console.log("DELETE request failed.");
+    				console.error(xhr.status + ': ' + xhr.responseText);
+    			}
+    		}
+    	};
+    	// 
+    	var userObjectJson = JSON.stringify(rowToDelete); 
+    	xhr.send(userObjectJson);
+    }); 
+}
+
+// table with all nutrition entities 
 function displayNutrition(nutrition) {
 	var dataDiv = document.getElementById('tableHere');
 
@@ -103,6 +261,7 @@ function displayNutrition(nutrition) {
 	} 
 }
 
+// create nutrition 
 document.getElementById("create").addEventListener('click', function(e) {
 	e.preventDefault();
 
@@ -128,24 +287,19 @@ document.getElementById("create").addEventListener('click', function(e) {
 		if (xhr.readyState === 4) {
 			if (xhr.status == 200 || xhr.status == 201) {
 				var data = JSON.parse(xhr.responseText);
-				console.log("test");
 				console.log(data);
-				console.log("test");
 				
 				var xhr2 = new XMLHttpRequest();
 				xhr2.open('GET', 'api/nutritioninfo');
 				xhr2.onreadystatechange = function() {
 					if (xhr2.readyState === 4 && xhr2.status === 200) {
 						var allEvents = JSON.parse(xhr2.responseText);
-						console.log("*** test ***");
 						displayNutrition(allEvents);
 					} else {
 						console.log("*** Data not found ***");
 					}
 				}
 				xhr2.send();
-				
-				
 			} else {
 				console.log("POST request failed.");
 				console.error(xhr.status + ': ' + xhr.responseText);
